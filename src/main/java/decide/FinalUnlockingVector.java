c6b767481295631aeb22d0eea3d286da57b1b6a8
Christofer Nolander <christofer.nolander@gmail.com>
2022-01-27 14:45:43 +0100

    feat: add Output class and skeleton for computations
    fixes #6
    
    Co-authored-by: dataekorre@hotmail.com <dataekorre@hotmail.com>
    Co-authored-by: psalqvist <psalqvist@users.noreply.github.com>
    Co-authored-by: Mark <mantaur@users.noreply.github.com>
    Co-authored-by: Kubha99 <Kubha99@users.noreply.github.com>

---
+++ src/main/java/decide/FinalUnlockingVector.java
@@ -0,0 +1,10 @@
+package decide;
+
+public class FinalUnlockingVector {
+    // Final Unlocking Conditions
+    public boolean[] conditions;
+
+    FinalUnlockingVector(Input input, PreliminaryUnlockingMatrix pum) {
+        this.conditions = new boolean[15];
+    }
+}
