31f7e73727be15f3361e727bcee5b4029949287f
Christofer Nolander <christofer.nolander@gmail.com>
2022-01-28 13:02:12 +0100

    feat: implement quadrants in LIC4
    fixes #15

--- src/test/java/decide/ConditionsMetVectorTest.java
+++ src/test/java/decide/ConditionsMetVectorTest.java
@@ -1,8 +1,44 @@
 package decide;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
 public class ConditionsMetVectorTest {
+    @Test
+    void LIC4() {
+        Input input = new Input();
+        input.parameters = input.new Parameters();
+        input.parameters.q_points = 3;
+        input.parameters.quads = 2;
+        input.points = new Point[]{
+            new Point(0, 0),
+            new Point(-1, 0),
+            new Point(0, -1),
+        };
+        assertTrue(ConditionsMetVector.LIC4(input));
+
+        // It is not possible for 2 points to be in 3 quadrants
+        input.parameters.q_points = 2;
+        assertFalse(ConditionsMetVector.LIC4(input));
+
+        // If we reduce the number of quadrants, it should be possible for 2 points
+        input.parameters.quads = 1;
+        assertTrue(ConditionsMetVector.LIC4(input));
+    }
+
+    @Test
+    void quadrant() {
+        // Extracted from the specification
+        assertEquals(0, ConditionsMetVector.quadrant(new Point(0, 0)));
+        assertEquals(1, ConditionsMetVector.quadrant(new Point(-1, 0)));
+        assertEquals(2, ConditionsMetVector.quadrant(new Point(0, -1)));
+        assertEquals(0, ConditionsMetVector.quadrant(new Point(0, 1)));
+        assertEquals(0, ConditionsMetVector.quadrant(new Point(1, 0)));
+
+        // Make sure that the last quadrant works
+        assertEquals(3, ConditionsMetVector.quadrant(new Point(1, -1)));
+    }
 }
