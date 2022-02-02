345a888aa1baf983683c77ab3f5a474d3eac960b
Christofer Nolander <christofer.nolander@gmail.com>
2022-02-02 12:51:37 +0100

    feat: add check if LCM is symmetric
    fixes #70

--- src/main/java/decide/PreliminaryUnlockingMatrix.java
+++ src/main/java/decide/PreliminaryUnlockingMatrix.java
@@ -8,6 +8,10 @@ public class PreliminaryUnlockingMatrix {
     }
 
     PreliminaryUnlockingMatrix(Input input, ConditionsMetVector cmv) {
+        if (!input.isLCMSymmetric()) {
+            throw new RuntimeException("`Input.lcm` is not symmetric");
+        }
+
         this.conditions = new boolean[15][15];
         for (int row = 0; row < 15; row++) {
             for (int col = row; col < 15; col++) {   // we only need to iterate over the upper triangle (cause symmetry)
